1332c93760e8999f9cd3aef51f25751d56d3cda9
family <family@familys-iMac.local>
2023-05-22 16:45:36 -0400

    Backend work

---
+++ server.js
@@ -0,0 +1,64 @@
+// const app = require("./app");
+// const port = 3000;
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// const app = require("./app");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+// app.use(bodyParser.json());
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Create a transporter with your email provider's SMTP settings
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "taylasouul@gmail.com",
+      pass: "sbmsqqqonnavsbdz",
+    },
+    debug: true,
+  });
+
+  // Prepare the email data
+  const mailOptions = {
+    from: `<${email}>`,
+    to: "taylasouul@gmail.com",
+    subject: "New Contact Form Submission",
+    replyTo: ` <${email}>`,
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Message: ${message}
+    `,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      console.log("Error sending email:", error.message);
+      console.log("Stack trace:", error.stack);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log(`Server listening on port ${port}`);
+});
